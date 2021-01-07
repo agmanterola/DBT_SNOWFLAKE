757e5bda0f29072d3c93c4a0dbf9a0d9ca25640a
Aitor Garcia <ragnar131@gmail.com>
2021-01-07 10:14:28 +0000

    Correccion del hub orders para el campo orderdate

--- models/vault/hub/hub_orders.sql
+++ models/vault/hub/hub_orders.sql
@@ -10,8 +10,7 @@ with hub_orders as (
     select
 		hash_orderkey,
         {{ make_tracefields(source='stg_orders') }},
-        ORDERKEY,
-        ORDERDATE
+        ORDERKEY
     from {{ ref('stg_orders') }}
     {% if is_incremental() %}
         where ORDERDATE >= (select max(ORDERDATE) from {{ this }})
